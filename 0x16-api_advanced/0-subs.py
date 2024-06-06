5360f426f24337831c73b9631c505d646abf4312
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-06-06 06:38:46 -0700

    dohard

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
+
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
@@ -10,7 +11,7 @@ def number_of_subscribers(subreddit):
                             .format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-if sub_info.status_code >= 300:
+    if sub_info.status_code >= 300:
         return 0
 
-return sub_info.json().get("data").get("subscribers")
+    return sub_info.json().get("data").get("subscribers")
